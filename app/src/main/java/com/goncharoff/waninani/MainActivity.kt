d0ccd9b9dfcbab8c38e1d6f1f35c6c664313ab3c
Nik <nik.ignition.work@gmail.com>
2021-02-09 20:03:36 +0200

    added api key

--- app/src/main/java/com/goncharoff/waninani/MainActivity.kt
+++ app/src/main/java/com/goncharoff/waninani/MainActivity.kt
@@ -2,10 +2,14 @@ package com.goncharoff.waninani
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
